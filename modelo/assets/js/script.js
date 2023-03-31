3b490d91fa6777018cf0575ed86485c8f2a71cef
matutaj <matutajorge2002@gmail.com>
2023-03-31 01:43:31 +0100

    portfolio Criado

---
+++ modelo/assets/js/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+  strings: ["FullStack Developer", "Designer Graphico", "Photographer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
